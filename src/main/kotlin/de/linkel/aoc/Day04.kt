fe6ca7cd5155ba4b195bf83e3ec1257b00391f57
Stephan Linkel <251381+norganos@users.noreply.github.com>
2024-12-04 06:33:01 +0100

    save commit: part B example works but finds wrong solution for my input

--- src/main/kotlin/de/linkel/aoc/Day04.kt
+++ src/main/kotlin/de/linkel/aoc/Day04.kt
@@ -2,6 +2,9 @@ package de.linkel.aoc
 
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
+import de.linkel.aoc.utils.grid.Grid
+import de.linkel.aoc.utils.grid.Point
+import de.linkel.aoc.utils.grid.Vector
 import jakarta.inject.Singleton
 import kotlin.math.pow
 
@@ -10,6 +13,67 @@ class Day04: AbstractLinesAdventDay<Long>() {
     override val day = 4
 
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return if (part == QuizPart.A)
+            partA(lines)
+        else
+            partB(lines)
+    }
+
+    fun partA(lines: Sequence<String>): Long {
+        val trails = mutableListOf<List<Pair<Point, Char>>>()
+        val grid = Grid.parse(lines) { pos, char ->
+            char.also { if (char == 'X') trails.add(listOf(pos to char)) }
+        }
+        val results = mutableListOf<List<Pair<Point, Char>>>()
+        while (trails.isNotEmpty()) {
+            val trail = trails.removeFirst()
+            val (pos, char) = trail.last()
+            if (trail.size == 1) {
+                grid.getNeighbours(pos, true).forEach { neighbour ->
+                    if (neighbour.data == 'M') {
+                        trails.add(trail + listOf(neighbour.point to neighbour.data))
+                    }
+                }
+            } else {
+                val searchPoint = pos + (pos - trail[trail.size - 2].first)
+                if (trail.size == 2) {
+                    if (searchPoint in grid && grid[searchPoint] == 'A') {
+                        trails.add(trail + listOf(searchPoint to 'A'))
+                    }
+                } else {
+                    if (searchPoint in grid && grid[searchPoint] == 'S') {
+                        results.add(trail + listOf(searchPoint to 'S'))
+                    }
+                }
+            }
+        }
+        return results.size.toLong()
+    }
+
+    fun partB(lines: Sequence<String>): Long {
+        val aList = mutableSetOf<Point>()
+        val grid = Grid.parse(lines) { pos, char ->
+            char.also { if (char == 'A') aList.add(pos) }
+        }
+        return aList
+            .map { pos ->
+                pos to listOf(
+                    pos + Vector(-1,-1),
+                    pos + Vector(1,-1),
+                    pos + Vector(1,1),
+                    pos + Vector(-1,1),
+                )
+            }
+            .filter { (_, neighbours) -> neighbours.all { it in grid } }
+            .count { (_, neighbours) ->
+                neighbours
+                    .map { grid[it] }
+                    .groupingBy { it }
+                    .eachCount()
+                    .let {
+                        it['M'] == 2 && it['S'] == 2
+                    }
+            }
+            .toLong()
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day04Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day04Test.kt
@@ -2,14 +2,24 @@ package de.linkel.aoc
 
 class Day04Test: AbstractDayTest<Long>() {
     override val exampleA = """
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 18L
+    override val solutionA = 2603L
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
+    override val exampleSolutionB = 9L
     override val solutionB = 0L
 
     override val implementation = Day04()
