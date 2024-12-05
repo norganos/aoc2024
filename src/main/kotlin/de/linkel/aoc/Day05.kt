2c4081f3e238e5c2328a98ee0c800f57d7fcfcda
Stephan Linkel <stephan@linkel.de>
2024-12-05 06:26:12 +0100

    cleanup up a lot of old imports from last year upgraded aoc-utils lib to 1.2.0 changed day 4 to use new discrete plain geometry classes

--- src/main/kotlin/de/linkel/aoc/Day05.kt
+++ src/main/kotlin/de/linkel/aoc/Day05.kt
@@ -3,15 +3,57 @@ package de.linkel.aoc
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
 import jakarta.inject.Singleton
-import java.lang.Exception
-import kotlin.math.max
-import kotlin.math.min
 
 @Singleton
 class Day05: AbstractLinesAdventDay<Long>() {
     override val day = 5
 
+    data class Buffer(
+        val rules: List<Pair<Long, Long>> = emptyList(),
+        val pages: List<List<Long>> = emptyList(),
+    )
+
+    class PageComparator(
+        rules: List<Pair<Long, Long>>
+    ): Comparator<Long> {
+        val before = rules.groupBy { (from, _) -> from }.mapValues { (_, v) -> v.map { it.second } }
+        val after = rules.groupBy { (_, to) -> to }.mapValues { (_, v) -> v.map { it.first } }
+
+        override fun compare(o1: Long?, o2: Long?): Int {
+            return if (before[o1]?.contains(o2) == true || after[o2]?.contains(o1) == true)
+                -1
+            else if (before[o2]?.contains(o1) == true || after[o1]?.contains(o2) == true)
+                1
+            else
+                0
+        }
+    }
+
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return lines.fold(Buffer()) { buffer, line ->
+            if (line.contains("|")) {
+                buffer.copy(
+                    rules = buffer.rules
+                            + (line.substringBefore("|").toLong() to line.substringAfterLast("|").toLong())
+                )
+            } else if (line.isNotEmpty()) {
+                buffer.copy(
+                    pages = buffer.pages + listOf(line.split(",").map(String::toLong))
+                )
+            } else buffer
+        }.let { buffer ->
+            if (part == QuizPart.A) {
+                buffer.pages.filter { pages ->
+                    pages == pages.sortedWith(PageComparator(buffer.rules))
+                }
+                    .sumOf { it[it.size / 2] }
+            } else {
+                buffer.pages.mapNotNull { pages ->
+                    pages.sortedWith(PageComparator(buffer.rules))
+                        .takeIf { it != pages }
+                }
+                    .sumOf { it[it.size / 2] }
+            }
+        }
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day05Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day05Test.kt
@@ -2,15 +2,43 @@ package de.linkel.aoc
 
 class Day05Test: AbstractDayTest<Long>() {
     override val exampleA = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 143L
+    override val solutionA = 4959L
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+    override val exampleSolutionB = 123L
+    override val solutionB = 4655L
 
     override val implementation = Day05()
 }
