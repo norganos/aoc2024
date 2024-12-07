18ac8da9785fe09aa30859bdefdf813a1c21fb29
Stephan Linkel <stephan@linkel.de>
2024-12-07 06:55:00 +0100

    day 7... took me 45min to discover that a "<" instead of a "<=" filtered out all lines that produce a correct result with a "* 1" at the end

--- src/main/kotlin/de/linkel/aoc/Day07.kt
+++ src/main/kotlin/de/linkel/aoc/Day07.kt
@@ -8,7 +8,59 @@ import jakarta.inject.Singleton
 class Day07: AbstractLinesAdventDay<Long>() {
     override val day = 7
 
+    interface Operator {
+        fun calculate(a: Long, b: Long): Long
+    }
+    object Plus: Operator {
+        override fun calculate(a: Long, b: Long): Long {
+            return a + b
+        }
+        override fun toString(): String {
+            return "+"
+        }
+    }
+    object Times: Operator {
+        override fun calculate(a: Long, b: Long): Long {
+            return a * b
+        }
+        override fun toString(): String {
+            return "*"
+        }
+    }
+    object Concat: Operator {
+        override fun calculate(a: Long, b: Long): Long {
+            return (a.toString() + b.toString()).toLong()
+        }
+        override fun toString(): String {
+            return "||"
+        }
+    }
+
+    fun testCalculation(operators: List<Operator>, desired: Long, result: Long, input: List<Long>): Boolean {
+        val tail = input.drop(1)
+        val operand = input.first()
+        for (operator in operators) {
+            val newResult = operator.calculate(result, operand)
+            if (newResult == desired && tail.isEmpty()) {
+                return true
+            } else if (newResult <= desired && tail.isNotEmpty() && testCalculation(operators, desired, newResult, tail)) {
+                return true
+            }
+        }
+        return false
+    }
+
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        val operators = if (part == QuizPart.A) listOf(Plus, Times) else listOf(Plus, Times, Concat)
+        return lines
+            .map { line ->
+                val tv = line.substringBefore(":").toLong()
+                val others = line.substringAfter(": ").split(" ").map { it.toLong() }
+                tv to others
+            }
+            .filter { (tv, vs) ->
+                testCalculation(operators, tv, vs.first(), vs.drop(1))
+            }
+            .sumOf { (tv, _) -> tv }
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day07Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day07Test.kt
@@ -2,15 +2,25 @@ package de.linkel.aoc
 
 class Day07Test: AbstractDayTest<Long>() {
     override val exampleA = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 3749L
+    override val solutionA = 1582598718861L
+                           //1582196515960L
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+    override val exampleSolutionB = 11387L
+    override val solutionB = 165278151522644L
 
     override val implementation = Day07()
 }
