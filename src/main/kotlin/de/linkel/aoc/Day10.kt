9c69f441cc48e2fce355bdac23cb033d6974177d
Stephan Linkel <stephan@linkel.de>
2024-12-10 06:19:39 +0100

    nice easy day 10

--- src/main/kotlin/de/linkel/aoc/Day10.kt
+++ src/main/kotlin/de/linkel/aoc/Day10.kt
@@ -2,6 +2,8 @@ package de.linkel.aoc
 
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
+import de.linkel.aoc.utils.geometry.plain.discrete.Point
+import de.linkel.aoc.utils.grid.Grid
 import jakarta.inject.Singleton
 
 @Singleton
@@ -9,6 +11,38 @@ class Day10: AbstractLinesAdventDay<Long>() {
     override val day = 10
 
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return Grid.parse(lines) { pos, char -> char.digitToInt() }
+            .let { grid ->
+                grid.getAllData()
+                    .filter { it.data == 0 }
+                    .flatMap { hike(grid, listOf(it.point)) }
+                    .let { trails ->
+                        if (part == QuizPart.A)
+                            trails
+                                .map { it.first() to it.last() }
+                                .toSet()
+                                .groupingBy { it.first }.eachCount()
+                                .entries
+                                .sumOf { it.value.toLong() }
+                        else
+                            trails
+                                .groupingBy { it.first() }
+                                .eachCount()
+                                .entries
+                                .sumOf { it.value.toLong() }
+                    }
+            }
+    }
+
+    private fun hike(grid: Grid<Int>, trail: List<Point>): Set<List<Point>> {
+        val height = grid[trail.last()]!!
+        return grid.getNeighbours(trail.last(), diagonal = false)
+            .filter { it.data == height + 1 }
+            .map { it to trail + it.point }
+            .flatMap { (dp, nt) ->
+                if (dp.data == 9) setOf(nt)
+                else hike(grid, nt)
+            }
+            .toSet()
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day10Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day10Test.kt
@@ -2,15 +2,23 @@ package de.linkel.aoc
 
 class Day10Test: AbstractDayTest<Long>() {
     override val exampleA = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 36L
+    override val solutionA = 811L
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+    override val exampleSolutionB = 81L
+    override val solutionB = 1794L
 
     override val implementation = Day10()
 }
