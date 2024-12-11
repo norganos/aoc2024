b7465527ec55be5c14337608f0c7b3b89a296a8a
Stephan Linkel <stephan@linkel.de>
2024-12-11 06:13:43 +0100

    naive day 11 implementation

--- src/main/kotlin/de/linkel/aoc/Day11.kt
+++ src/main/kotlin/de/linkel/aoc/Day11.kt
@@ -8,7 +8,30 @@ import jakarta.inject.Singleton
 class Day11: AbstractLinesAdventDay<Long>() {
     override val day = 11
 
+    fun blink(value: Long): List<Long> {
+        val strValue = value.toString()
+        return if (value == 0L)
+            listOf(1L)
+        else if(strValue.length % 2 == 0)
+            listOf(
+                strValue.substring(0, strValue.length / 2).toLong(),
+                strValue.substring(strValue.length / 2).toLong()
+            )
+        else
+            listOf(value * 2024L)
+    }
+
+    private val whitespace = Regex("\\s+")
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return (if (part == QuizPart.A) (0 until 25) else (0 until 75))
+            .fold(
+                lines.flatMap { line ->
+                    line.split(whitespace).map { it.toLong() }
+                }
+                    .toList()
+            ) { stones, _ ->
+                stones.flatMap { blink(it) }
+            }
+            .size.toLong()
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day11Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day11Test.kt
@@ -2,9 +2,10 @@ package de.linkel.aoc
 
 class Day11Test: AbstractDayTest<Long>() {
     override val exampleA = """
+125 17
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 55312L
+    override val solutionA = 185894L
 
 
 //    override val exampleB = """
