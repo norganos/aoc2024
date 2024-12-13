de72df255c4ce7e3ffb1fbf552b141af7f975c54
Stephan Linkel <stephan@linkel.de>
2024-12-13 08:27:10 +0100

    day 13 with cramer rule

--- src/main/kotlin/de/linkel/aoc/Day13.kt
+++ src/main/kotlin/de/linkel/aoc/Day13.kt
@@ -6,44 +6,86 @@ import de.linkel.aoc.utils.geometry.plain.discrete.Point
 import de.linkel.aoc.utils.geometry.plain.discrete.Vector
 import de.linkel.aoc.utils.iterables.split
 import jakarta.inject.Singleton
+import java.math.BigDecimal
 
 @Singleton
-class Day13: AbstractLinesAdventDay<Long>() {
+class Day13: AbstractLinesAdventDay<BigDecimal>() {
     override val day = 13
 
     private val buttonRegex = Regex("Button [AB]: X([-+]\\d+), Y([-+]\\d+)")
     private val prizeRegex = Regex("Prize: X=(\\d+), Y=(\\d+)")
 
-    data class Machine(
-        val a: Vector,
-        val b: Vector,
-        val prize: Point
-    )
-    data class Play(
-        val a: Int,
-        val b: Int
+    data class LongVector(
+        val x: BigDecimal,
+        val y: BigDecimal
     ) {
-        val cost = 3 * a + b
-        fun wins(machine: Machine): Boolean
-            = Point.ZERO + machine.a * a + machine.b * b == machine.prize
+        operator fun plus(other: LongVector) = LongVector(x + other.x, y + other.y)
+        operator fun times(amount: BigDecimal) = LongVector(x * amount, y * amount)
     }
 
-    private fun parseButton(input: String): Vector {
+    data class Machine(
+        val a: LongVector,
+        val b: LongVector,
+        val p: LongVector
+    ) {
+        fun solve(): Play? {
+            fun det(a: BigDecimal, b: BigDecimal, c: BigDecimal, d: BigDecimal)
+                = a * d - b * c
+            return try {
+                val ax = a.x
+                val ay = a.y
+                val bx = b.x
+                val by = b.y
+                val mx = p.x
+                val my = p.y
+
+                val dab = det(ax, bx, ay, by)
+                val dmb = det(mx, bx, my, by)
+                val dam = det(ax, mx, ay, my)
+                if (dmb % dab == BigDecimal.ZERO && dam % dab == BigDecimal.ZERO) {
+                    val a = dmb / dab
+                    val b = dam / dab
+                    Play(a, b)
+                } else null
+
+
+
+
+//                val b = (my - ((ay * mx) / ax)) / (by - ((ay * bx) / ax))
+//                val a = (mx - bx * b) / ax
+//
+//                Play(a, b)
+//                    .takeIf { it.a >= BigDecimal.ZERO && it.b >= BigDecimal.ZERO && it.wins(this) }
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+    data class Play(
+        val a: BigDecimal,
+        val b: BigDecimal
+    ) {
+        val cost = a + a + a + b
+        fun wins(machine: Machine): Boolean
+            = machine.a * a + machine.b * b == machine.p
+    }
+
+    private fun parseButton(input: String): LongVector {
         return buttonRegex.matchEntire(input)!!
             .groupValues
-            .let { (_, x, y) -> Vector(x.toInt(), y.toInt()) }
+            .let { (_, x, y) -> LongVector(x.toBigDecimal(), y.toBigDecimal()) }
     }
-    private fun parsePrize(input: String): Point {
+    private fun parsePrize(input: String): LongVector {
         return prizeRegex.matchEntire(input)!!
             .groupValues
-            .let { (_, x, y) -> Point(x.toInt(), y.toInt()) }
+            .let { (_, x, y) -> LongVector(x.toBigDecimal(), y.toBigDecimal()) }
     }
 
     private fun findBestPlay(machine: Machine): Play? {
         var bestPlay: Play? = null
         (0..100).forEach { aPresses ->
             (0..100).forEach { bPresses ->
-                val play = Play(aPresses, bPresses)
+                val play = Play(aPresses.toBigDecimal(), bPresses.toBigDecimal())
                 if (play.wins(machine)) {
                     if (bestPlay?.takeIf { it.cost < play.cost } == null) {
                         bestPlay = play
@@ -54,7 +96,9 @@ class Day13: AbstractLinesAdventDay<Long>() {
         return bestPlay
     }
 
-    override fun process(part: QuizPart, lines: Sequence<String>): Long {
+    override fun process(part: QuizPart, lines: Sequence<String>): BigDecimal {
+        val offset = if (part == QuizPart.B) LongVector(BigDecimal("10000000000000"), BigDecimal("10000000000000")) else LongVector(
+            BigDecimal.ZERO, BigDecimal.ZERO)
         return lines
             .split { it.isEmpty() }
             .map { machineLines ->
@@ -65,10 +109,20 @@ class Day13: AbstractLinesAdventDay<Long>() {
                 Machine(
                     a = parseButton(machineLines[0]),
                     b = parseButton(machineLines[1]),
-                    prize = parsePrize(machineLines[2]),
+                    p = parsePrize(machineLines[2]) + offset
                 )
             }
-            .mapNotNull { findBestPlay(it)?.cost }
-            .sumOf { it.toLong() }
+            .mapNotNull {
+                val solved = it.solve()
+                if (part == QuizPart.A && solved == null) {
+                    val brute = findBestPlay(it)
+                    if (brute != null) {
+                        println("$it -> $solved / $brute")
+                        it.solve()
+                    }
+                }
+                solved
+            }
+            .sumOf { it.cost }
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day13Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day13Test.kt
@@ -1,6 +1,8 @@
 package de.linkel.aoc
 
-class Day13Test: AbstractDayTest<Long>() {
+import java.math.BigDecimal
+
+class Day13Test: AbstractDayTest<BigDecimal>() {
     override val exampleA = """
 Button A: X+94, Y+34
 Button B: X+22, Y+67
@@ -18,14 +20,14 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
         """.trimIndent()
-    override val exampleSolutionA = 480L
-    override val solutionA = 27105L
+    override val exampleSolutionA = BigDecimal("480")
+    override val solutionA = BigDecimal("27105")
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+    override val exampleSolutionB = BigDecimal("875318608908")
+    override val solutionB = BigDecimal("101726882250942")
 
     override val implementation = Day13()
 }
