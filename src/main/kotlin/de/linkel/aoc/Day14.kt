cb7123e28005e500400dbba0945dfd84cefe785a
Stephan Linkel <stephan@linkel.de>
2024-12-14 08:16:24 +0100

    day 14... part 1 is a nice clean functional solution, part 2 was solved visually and is a mess

--- src/main/kotlin/de/linkel/aoc/Day14.kt
+++ src/main/kotlin/de/linkel/aoc/Day14.kt
@@ -2,13 +2,124 @@ package de.linkel.aoc
 
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
+import de.linkel.aoc.utils.geometry.plain.discrete.Dimension
+import de.linkel.aoc.utils.geometry.plain.discrete.Vector
+import de.linkel.aoc.utils.geometry.plain.discrete.Point
+import de.linkel.aoc.utils.geometry.plain.discrete.Rectangle
 import jakarta.inject.Singleton
+import java.io.File
 
 @Singleton
 class Day14: AbstractLinesAdventDay<Long>() {
     override val day = 14
 
+    private val regex = Regex("""^p=(-?\d+),\s?(-?\d+)\s?v=(-?\d+),\s?(-?\d+)$""")
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return lines
+            .map { line ->
+                regex.matchEntire(line)!!
+                    .groupValues
+                    .let {
+                        Point(it[1].toInt(), it[2].toInt()) to Vector(it[3].toInt(), it[4].toInt())
+                    }
+            }
+            .toList()
+            .let { robots ->
+                val area = if (robots.size < 20)
+                    Rectangle(0, 0, 11, 7)
+                else
+                    Rectangle(0, 0, 101, 103)
+                val destAreas = listOf(
+                    Rectangle(
+                        area.x, area.y,
+                        area.width / 2, area.height / 2
+                    ),
+                    Rectangle(
+                        area.x + (area.width + 1) / 2, area.y,
+                        area.width / 2, area.height / 2
+                    ),
+                    Rectangle(
+                        area.x, area.y + (area.height + 1) / 2,
+                        area.width / 2, area.height / 2
+                    ),
+                    Rectangle(
+                        area.x + (area.width + 1) / 2, area.y + (area.height + 1) / 2,
+                        area.width / 2, area.height / 2
+                    ),
+                )
+
+                if (part == QuizPart.A) {
+                    robots
+                        .transform(100) { list, i ->
+                            list.map { (p, v) ->
+                                (p + v) % area to v
+                            }
+                        }
+                        .mapNotNull { (p, _) ->
+                            destAreas.firstOrNull { p in it }
+                        }
+                        .groupingBy { it }
+                        .eachCount()
+                        .entries
+                        .fold(1L) { p, e -> p * e.value }
+                } else {
+                    val pre = if (robots.size < 20) "e" else "s"
+                    robots
+                        // visually (using the txt files below) found first match at 7131 (then cycling with length 10403)
+                        .transform(7132) { list, i ->
+                            list
+                                .map { (p, v) ->
+                                    (p + v) % area to v
+                                }
+                                .also { rrr ->
+                                    if (File(pre).exists())
+                                        File("$pre/${i.toString().padStart(6, '0')}.txt").writer().use { w ->
+                                            rrr
+                                                .map { it.first }
+                                                .distinct()
+                                                .sortedWith(compareBy({ it.y }, { it.x }))
+                                                .fold(Point.ZERO) { last, p ->
+                                                    (p.y - last.y).times {
+                                                        w.append("\n")
+                                                    }
+                                                    (p.x - (last.x.takeIf { p.y == last.y } ?: 0)).times {
+                                                        w.append(" ")
+                                                    }
+                                                    w.append("#")
+                                                    p
+                                                }
+                                        }
+                                }
+                        }
+                    // I hate myself for this...
+                    7132L
+                }
+
+            }
+    }
+
+    private fun <T> T.transform(times: Int, lambda: (T, Int) -> T): T {
+        return (0 until times)
+            .fold(this) { acc, i ->
+            lambda(acc, i)
+        }
+    }
+    private fun <T> List<T>.transformElements(times: Int, lambda: (T, Int) -> T): List<T> {
+        return (0 until times)
+            .fold(this) { acc, i ->
+            acc.map { lambda(it, i) }
+        }
+    }
+    private fun Int.times(lambda: (Int) -> Unit) {
+        return (0 until this)
+            .forEach(lambda)
+    }
+
+    private operator fun Point.rem(rect: Rectangle): Point {
+        return if (this in rect) this else
+            Point(
+                x = (((this.x - rect.x) % rect.width) + rect.width) % rect.width + rect.x,
+                y = (((this.y - rect.y) % rect.height) + rect.height) % rect.height  + rect.y
+            )
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day14Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day14Test.kt
@@ -2,15 +2,26 @@ package de.linkel.aoc
 
 class Day14Test: AbstractDayTest<Long>() {
     override val exampleA = """
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
-
+    override val exampleSolutionA = 12L
+    override val solutionA = 229980828L
 
 //    override val exampleB = """
-//        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+//        """.trimIndent() 79952 90355 7131
+    override val exampleSolutionB = 7132L
+    override val solutionB = 7132L
 
     override val implementation = Day14()
 }
