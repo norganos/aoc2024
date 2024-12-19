75dadc44603ef363b76dd0a55293ca11ab16e8f9
Stephan Linkel <stephan@linkel.de>
2024-12-19 06:52:02 +0100

    day 19

--- src/main/kotlin/de/linkel/aoc/Day19.kt
+++ src/main/kotlin/de/linkel/aoc/Day19.kt
@@ -2,6 +2,7 @@ package de.linkel.aoc
 
 import de.linkel.aoc.base.AbstractLinesAdventDay
 import de.linkel.aoc.base.QuizPart
+import de.linkel.aoc.utils.iterables.inTwoBlocks
 import jakarta.inject.Singleton
 
 @Singleton
@@ -9,6 +10,42 @@ class Day19: AbstractLinesAdventDay<Long>() {
     override val day = 19
 
     override fun process(part: QuizPart, lines: Sequence<String>): Long {
-        return lines.count().toLong()
+        return lines
+            .inTwoBlocks(
+                delimiterPredicate = { it.isEmpty() },
+                block1 = { blockLines ->
+                    blockLines.first()
+                        .split(", ")
+                        .toSet()
+                },
+                block2 = { towels, blockLines ->
+                    if (part == QuizPart.A)
+                        blockLines
+                            .count {
+                                this.testDesign(towels, it) >= 1L
+                            }
+                            .toLong()
+                    else
+                        blockLines
+                            .sumOf {
+                                this.testDesign(towels, it)
+                            }
+                }
+            )
+    }
+
+    private var cache = mutableMapOf<String,Long>()
+    private fun testDesign(towels: Set<String>, pattern: String): Long {
+        return if (pattern.isEmpty()) 1L
+        else
+            cache[pattern] ?:
+            towels
+                .filter { pattern.startsWith(it) }
+                .sumOf {
+                    this.testDesign(towels, pattern.drop(it.length))
+                }
+                .also {
+                    cache[pattern] = it
+                }
     }
 }
--- src/test/kotlin/de/linkel/aoc/Day19Test.kt
+++ src/test/kotlin/de/linkel/aoc/Day19Test.kt
@@ -2,15 +2,25 @@ package de.linkel.aoc
 
 class Day19Test: AbstractDayTest<Long>() {
     override val exampleA = """
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
         """.trimIndent()
-    override val exampleSolutionA = 0L
-    override val solutionA = 0L
+    override val exampleSolutionA = 6L
+    override val solutionA = 367L
 
 
 //    override val exampleB = """
 //        """.trimIndent()
-    override val exampleSolutionB = 0L
-    override val solutionB = 0L
+    override val exampleSolutionB = 16L
+    override val solutionB = 724388733465031L
 
     override val implementation = Day19()
 }
